9beedea2f08378801909e6f9415e1a47409bbe57
Ahmed Tarek Zaher <122125653+ahmed-tarek-2004@users.noreply.github.com>
2025-11-06 21:50:06 +0200

    Update VirualTryOnScript.py

--- VirualTryOnScript.py
+++ VirualTryOnScript.py
@@ -6,14 +6,12 @@ import os
 import time
 import requests
 
-image1_path = r"E:\\VSC_ Codes\\python\\ME.jpg"
-image2_path = r"E:\\VSC_ Codes\\python\\ahmed.jpg"
+image1_path = os.getcwd()+r"\\ME.jpg"
+image2_path = os.getcwd()+r"\\ahmed.jpg"
+# print(image1_path)
 
 options = webdriver.ChromeOptions()
-options.add_argument("--headless=new")  
-options.add_argument("--window-size=1920,1080")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--start-maximized")
 driver = webdriver.Chrome(options=options)
 driver.get("https://huggingface.co/spaces/Kwai-Kolors/Kolors-Virtual-Try-On")
 
@@ -55,21 +53,25 @@ print(" Running model...")
 run_button = wait.until(EC.element_to_be_clickable((By.ID, "button")))
 run_button.click()
 
+
+
+
 print(" Waiting for result image...")
-result_img = wait.until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, "img.svelte-1pijsyv"))
-)
-print("Result appeared!")
-
-
-print(" Waiting for garment image to appear...")
-
-garment_img = wait.until(
-    EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.image-frame img[loading='lazy'].svelte-1pijsyv"))
-)[-1]
-
-wait.until(lambda d: garment_img.get_attribute("src") and "tmp" in garment_img.get_attribute("src"))
-print("Garment image loaded")
+try:
+    result_img = wait.until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "img[loading='lazy'].svelte-1pijsyv"))
+    )
+    print("Result appeared!")
+except:
+  run_button = wait.until(EC.element_to_be_clickable((By.ID, "button")))
+  run_button.click()
+  print(" Waiting for garment image to appear...")
+  
+  result_img = wait.until(
+      EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.image-frame img[loading='lazy'].svelte-1pijsyv"))
+  )[-1]
+  wait.until(lambda d: result_img.get_attribute("src") and "tmp" in result_img.get_attribute("src"))
+  print("Garment image loaded")
 
 
 
@@ -83,7 +85,7 @@ print("Garment image loaded")
 # driver.save_screenshot(screenshot_path)
 # print(f" Screenshot saved as {screenshot_path}")
 
-img_url = garment_img.get_attribute("src")
+img_url = result_img.get_attribute("src")
 print(" Image URL:", img_url)
 
 response = requests.get(img_url)
